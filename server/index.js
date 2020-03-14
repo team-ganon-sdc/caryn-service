d33949b0e1d280760d661d74cdc8fcb81af833db
Mason Borchard <masonborchard@gmail.com>
2020-03-14 14:40:56 -0700

    database and server are communicating

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const app = express();
 const PORT = process.env.PORT || 3003;
 const bodyParser = require('body-parser');
-const {AppImagesModel, AppImagesSchema, findOne, findAll, insertOne} = require('../database/models/Carousel.js');
+// const {AppImagesModel, AppImagesSchema, findOne, findAll, insertOne} = require('../database/models/Carousel.js');
 
-const app = express();
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb://localhost/carousel';
 var router = express.Router();
@@ -12,13 +12,13 @@ var router = express.Router();
 
 
 // open the default mongo connection
-mongoose.connect(mongoDB, {useMongoClient: true}, function(err, success) {
-  if (err) {
-    // console.log(err, 'error connecting to mongoDB');
-  } else {
-    // console.log("connected to mongoDB");
-  }
-});
+// mongoose.connect(mongoDB, function(err, success) {
+//   if (err) {
+//     // console.log(err, 'error connecting to mongoDB');
+//   } else {
+//     // console.log("connected to mongoDB");
+//   }
+// });
 
 
 // mongoose.connect(mongoDB, {useMongoClient: true}, function(err, success) {
@@ -35,31 +35,22 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-
-
-
-app.get('/carousel', (req, res) => {
-  // res.send({working: true})
-  // res.sendFile(path, join(__dirname + '/../client/public/index.html'))
-  AppImagesModel.find({id: 12}, (err, data) => {
-    if (err){
-      console.log(`error w get req ${err}`)
-    }
-    console.log(`!!!!!!!!!64646464 data is ${data}`);
-    res.send(data);
-  })
+app.get('/carousel', function(req, res) {
+  const clientPath = path.join(__dirname, '../public/index.html');
+  res.sendFile(clientPath);
 });
 
 
+// app.post('/carousel', (req, res) => {
+//   var images = req.body;
+//   console.log('!!!!!!!!req.body: ' + JSON.stringify(req.body));
 
-
-app.post('/carousel', (req, res) => {
-  var images = req.body;
-  console.log('!!!!!!!!req.body: ' + JSON.stringify(req.body));
-
-})
+// })
 
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+
+module.exports = app;
--- server/server.test.js
+++ server/server.test.js
@@ -1,20 +1,75 @@
-const supertest = require('supertest');
-const app = require('./index.js');
-const request = require(supertest)(app);
+// const supertest = require('supertest');
+// const express = require('express');
+const server = require('./index.js');
+// const app = express();
+const polyfill = require('babel-polyfill');
+const request = require('supertest');
 
-describe('jest test client', () => {
 
-  it('Gets the test endpoint', async done => {
-    // Sends GET Request to /test endpoint
-    const res = await request.get('/')
-    expect(response.status).toBe(200)
-    expect(response.body["what time is it?"]).toBe("time 4 broccoli")
-    done()
-  });
+
+describe('server tests', () => {
 
   test('jest should work correctly', () => {
     expect(5 + 5).toBe(10);
+  })
+
+  test('responds to GET', done => {
+    // Sends GET Request to /test endpoint
+    request(server)
+   .get('/carousel')
+   .then(response => {
+     expect(response.statusCode).toBe(200);
+     done();
+   });
+
+    // expect(typeof response.body).toBe('object')
+
   });
 
-});
+  test('should respond with correct values', () => {
+    return request(server)
+      .get('/carousel/:images')
+      .expect(200)
+      .expect(res => {
+        const data = res.body.data[0];
+
+        expect(data.id).toEqual(expect.any(Number));
+        expect(data.id).toBeGreaterThanOrEqual(1);
+        expect(data.id).toBeLessThanOrEqual(100);
+
+        expect(data.server_description).toEqual(expect.any(String));
+
+        expect(data.images[0]).toEqual(expect.any(String));
+      });
+  });
+
+
+
+})
+
+
+
+// const supertest = require('supertest');
+// const serverIndex= require('./index.js');
+// // const app = require('./index.js');
+// // const request = require(supertest)(app);
+
+// describe('jest test client', () => {
+
+//   it('Testing to see if Jest works', () => {
+//     expect(1).toBe(2)
+//   })
+//   // test('should use GET and get a response', async done => {
+//   //   // Sends GET Request to /test endpoint
+//   //   const res = await request.get('/')
+//   //   expect(response.status).toBe(200)
+//   //   // expect(response.body.id).toBe(12)
+//   //   done()
+//   // });
+
+//   // test('jest should work correctly', () => {
+//   //   expect(5 + 5).toBe(10);
+//   // });
+
+// });
 
