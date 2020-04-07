bd47e438cb4fb22dc6c55bad30d156bd36644a98
Caryn Shu <bugbugshu@gmail.com>
2020-04-06 17:54:35 -0700

    querying from cassandra

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
-const Carousels = require('../database/model.js');
+// const Carousels = require('../database/model.js');
 const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+// const db = require('../database/index.js');
 const app = express();
 const PORT =  3003;
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+    contactPoints: ['127.0.0.1:9042'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'apppreview',
+});
+client.connect(function (err) {
+  console.error(err);
+});
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -20,55 +30,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/carousels/:id', (req, res) => {
-  Carousels.find({id: req.params.id}, (err, results) => {
+  const query = `SELECT * FROM apppreview WHERE id = ${req.params.id}`;
+  client.execute(query, (err, result) => {
     if (err) {
-      return console.log('error getting from db: ', err)
-    }
-    res.json(results)
-  })
-})
-
-app.post('/carousels', function(req, res) {
-  let newCarousel = new Carousels({
-    id: req.body.id,
-    images: req.body.images,
-    app_description: req.body.description,
-    additional_text: req.body.additionalText
-  })
-
-  newCarousel.save((err, result) => {
-    if (err) {
-      return console.log(err)
+      console.error(err)
     } else {
-      res.send(result)
+      res.json(result.rows)
     }
   })
 })
 
-app.put('/carousels/:id', (req, res) => {
-  let description = req.body.description;
-  let additionalText = req.body.additionalText;
-  let update = {
-    app_description: description,
-    additional_text: additionalText
-  }
-  Carousels.findOneAndUpdate({id: req.params.id}, update, (err, results) => {
-    if (err) {
-      return console.log('error updating to db: ', err)
-    }
-    res.send(results);
-  })
-})
+// app.post('/carousels', function(req, res) {
+//   let newCarousel = new Carousels({
+//     id: req.body.id,
+//     images: req.body.images,
+//     app_description: req.body.description,
+//     additional_text: req.body.additionalText
+//   })
 
-app.delete('/carousels/:id', (req, res) => {
-  Carousels.findOneAndDelete({id: req.params.id}, (err, result) => {
-    if (err) {
-      return console.log('error deleting from db: ', err)
-    } else {
-      res.send(result)
-    }
-  })
-})
+//   newCarousel.save((err, result) => {
+//     if (err) {
+//       return console.log(err)
+//     } else {
+//       res.send(result)
+//     }
+//   })
+// })
+
+// app.put('/carousels/:id', (req, res) => {
+//   let description = req.body.description;
+//   let additionalText = req.body.additionalText;
+//   let update = {
+//     app_description: description,
+//     additional_text: additionalText
+//   }
+//   Carousels.findOneAndUpdate({id: req.params.id}, update, (err, results) => {
+//     if (err) {
+//       return console.log('error updating to db: ', err)
+//     }
+//     res.send(results);
+//   })
+// })
+
+// app.delete('/carousels/:id', (req, res) => {
+//   Carousels.findOneAndDelete({id: req.params.id}, (err, result) => {
+//     if (err) {
+//       return console.log('error deleting from db: ', err)
+//     } else {
+//       res.send(result)
+//     }
+//   })
+// })
 
 
 app.listen(PORT, () => {
