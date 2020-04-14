cd04aec2aa6497dd16ad23f6f3cd191f07d0c631
Caryn Shu <bugbugshu@gmail.com>
2020-04-14 14:29:49 -0700

    minor fix to work with proxy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App0 from './components/App0.jsx';
 
-ReactDOM.render(<App0 />, document.getElementById('app0'));
+ReactDOM.render(<App0 />, document.getElementById('app1'));
 
 
 // window.App0 = App0;
